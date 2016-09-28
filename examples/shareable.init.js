4aa58379b77788e7cfb9684f66b5c1b67e3b15c3
khai96_ <hvksmr1996@gmail.com>
2016-09-28 12:37:38 +0700

    Store things

--- examples/shareable.init.js
+++ examples/shareable.init.js
@@ -1,1 +1,23 @@
 'use strict'
+
+/* IMPORT */
+
+const {packages} = global.atom
+const {registerSpawnCommand} = packages.getLoadedPackage('quick-spawn').api
+
+/* DO THINGS */
+
+registerBash()
+
+/* DEFINE HOW TO DO THINGS */
+
+function registerBash () {
+  const bashSpawnSubscription = registerSpawnCommand({
+    execCmd: 'bash',
+    workingDirectory: getWorkingDirectory(), // optional, determines Bash's first working-directory, you can change it later by 'cd new-wdir'
+    attached: false, // optional, default to false, determines whether Bash process should be attached to Atom process
+    suspended: true, // optional, default to true; if false, Bash would be started right after quick-spawn's activation
+    __proto__: null // optional (of course), avoid the mess from Object.prototype
+  })
+  return bashSpawnSubscription
+}
