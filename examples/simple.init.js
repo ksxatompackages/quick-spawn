6628380968eb6bf5b6436533b59710c3a8133eaf
khai96_ <hvksmr1996@gmail.com>
2016-10-02 13:13:48 +0700

    Documenting

--- examples/simple.init.js
+++ examples/simple.init.js
@@ -15,19 +15,29 @@ registerWorkspaceBash()
 /* DEFINE HOW TO DO THINGS */
 
 // DESCRIPTION: registerWorkspaceBash
+//  → Distinct:
+//   * Registers for workspace
+//   * Uses getTabDirectory() as first current-working-directory
+//  → Returns 1 subscription
 function registerWorkspaceBash () {
   return registerSpawnCommand({
-    execCmd: 'bash',
-    atomCmd: 'simple-bash:workspace',
-    atomTarget: 'atom-workspace',
-    workingDirectory: () => workspace.getActivePaneItem().getDirectoryPath(),
-    attached: false,
-    suspended: true,
-    viewStdIO: ['stdin', 'stdout', 'stderr'],
-    type: 'tab',
-    atomKeybinding: 'ctrl-shift-b s w',
-    exitOnClose: true,
-    closeOnExit: true,
-    detachedTextBox: 'none'
+    execCmd: 'bash', // required; program needs to be executed
+    atomCmd: 'simple-bash:workspace', // required; registers an atom-command, appears as "Simple Bash: Workspace" in Command-Palette
+    atomTarget: 'atom-workspace', // required; determines where to attach the atom-command
+    workingDirectory: getTabDirectory, // optional; determines Bash's first working-directory, you can change it later by 'cd new-wdir'
+    attached: false, // optional, default to false; determines whether Bash process should be attached to Atom process
+    suspended: true, // optional, default to true; if false, Bash would be started right after quick-spawn's activation
+    viewStdIO: ['stdin', 'stdout', 'stderr'], // optional, determines whether stdin, stdout and stderr should be shown; shows all three by default
+    type: 'tab', // optional, default to 'tab'
+    atomKeybinding: 'ctrl-shift-b s w', // optional
+    exitOnClose: true, // optional, default to true; should the spawned process be terminated when close the view
+    closeOnExit: true, // optional, default to false; should the view be closed when bash exits
+    detachedTextBox: 'none', // optional, default to 'none'
+    __proto__: null // optional (of course); avoid the mess from Object.prototype
   })
+}
+
+// DESCRIPTION: getTabDirectory
+function getTabDirectory () {
+  return workspace.getActivePaneItem().getDirectoryPath()
 }
