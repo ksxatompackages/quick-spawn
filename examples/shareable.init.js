60146bbe5f01a2e5034dd94b9c355c9f98aa5507
khai96_ <hvksmr1996@gmail.com>
2016-10-02 13:00:47 +0700

    Fix English, again

--- examples/shareable.init.js
+++ examples/shareable.init.js
@@ -44,7 +44,7 @@ function registerCentralizedBash () {
     __proto__: null // optional (of course); avoid the mess from Object.prototype
   })
   const mainAtomCommandSubscription = spawnSubscription.registerAtomCommand({
-    atomCmd: 'view-bash:main', // required; register an Atom command, appear as "View Bash: Main" in Command-Palette
+    atomCmd: 'view-bash:main', // required; register an Atom command, appears as "View Bash: Main" in Command-Palette
     atomTarget: 'atom-workspace', // required; determines where to attach the atom-command
     type: 'tab', // optional, default to 'tab'
     viewStdIO: ['stdin', 'stdout', 'stderr'], // optional, determines whether stdin, stdout and stderr should be shown; shows all three by default
