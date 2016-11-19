953c231f0ae95934ff91997f72f8bf2145d6cfbc
khai96_ <hvksmr1996@gmail.com>
2016-11-19 17:15:35 +0700

    Syntax error

--- test/main/templates/disposable.js
+++ test/main/templates/disposable.js
@@ -7,7 +7,7 @@ function main (Disposable) {
   const applyClass = require(join(object, '../../utils/apply-class.js'))
   let out = null
   const dispose = () => { out = 'expected' }
-  class MainDisposable extends applyClass(class {}, [Disposable({dispose})])
+  class MainDisposable extends applyClass(class {}, [Disposable({dispose})]) {}
   const disposable = new MainDisposable()
   strictEqual(disposable.disposed, false, 'Initially, disposable.disposed must be false')
   for (let count = 5; count; --count) {
