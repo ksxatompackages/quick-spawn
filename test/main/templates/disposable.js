cbba0f4fdd704c868790799cbe5e85a8bd3ce833
khai96_ <hvksmr1996@gmail.com>
2016-11-21 15:48:44 +0700

    Save my work

--- test/main/templates/disposable.js
+++ test/main/templates/disposable.js
@@ -6,8 +6,12 @@ const {strictEqual} = require('assert')
 function main (Disposable, {object}) {
   const applyClass = require(join(object, '../../utils/apply-class.js'))
   let out = null
-  const dispose = () => { out = 'expected' }
-  class MainDisposable extends applyClass(class {}, [Disposable({dispose})]) {}
+  class MainDisposable extends applyClass(class {}, [Disposable]) {
+    constructor () {
+      const dispose = () => { out = 'expected' }
+      super({dispose})
+    }
+  }
   const disposable = new MainDisposable()
   strictEqual(disposable.disposed, false, 'Initially, disposable.disposed must be false')
   for (let count = 5; count; --count) {
@@ -16,5 +20,19 @@ function main (Disposable, {object}) {
   }
   return [disposable]
 }
+
+// function main (Disposable, {object}) {
+//   const applyClass = require(join(object, '../../utils/apply-class.js'))
+//   let out = null
+//   const dispose = () => { out = 'expected' }
+//   class MainDisposable extends applyClass(class {}, [Disposable({dispose})]) {}
+//   const disposable = new MainDisposable()
+//   strictEqual(disposable.disposed, false, 'Initially, disposable.disposed must be false')
+//   for (let count = 5; count; --count) {
+//     disposable.dispose()
+//     strictEqual(disposable.disposed, true, 'At any time after calling dispose, disposable.disposed must always be false')
+//   }
+//   return [disposable]
+// }
 
 module.exports = main
